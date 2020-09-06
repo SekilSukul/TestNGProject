e293f1fb40ff45b6b6ae4867f8a556e427851c95
SekilSukul <ahmttrk45@gmail.com>
2020-09-06 16:49:50 +0200

    Day13

---
+++ src/test/java/com/techproed/tests/WindowHandleTest.java
@@ -0,0 +1,44 @@
+package com.techproed.tests;
+
+import com.techproed.utulities.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import java.util.Set;
+
+public class WindowHandleTest extends TestBase {
+
+    @Test
+    public void coklupencereTest(){
+        driver.get("https://the-internet.herokuapp.com/windows");
+
+        String ilksayfahandle = driver.getWindowHandle();
+        System.out.println(ilksayfahandle);
+
+        driver.findElement(By.partialLinkText("Click Here")).click();
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        Set <String> tumPencereler =  driver.getWindowHandles();
+            for (String handle : tumPencereler){
+                System.out.println(handle);
+            }
+
+
+        String ikincisayfahandle = (String) ((tumPencereler.toArray())[tumPencereler.size()-1]);
+        System.out.println(ikincisayfahandle);
+
+        driver.switchTo().window(ilksayfahandle);
+
+
+
+    }
+
+
+}
